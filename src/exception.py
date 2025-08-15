e2c3226a58808803bf31420b1a86cb6684436c9b
dhailu <hailudjj@gmail.com>
2025-08-15 15:26:14 -0700

    EAD and model file hadded

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -15,3 +16,10 @@ class CustomeException(Exception):
 
     def __str__(self):
         return self.error_message
+    
+if __name__ == "__main__":
+    try:
+        1/0
+    except Exception as e:
+        logging.info("divided by zero")
+        raise CustomeException(e, sys)
